fe6fcca6463b731a749d13cb0d16cba476c00a29
Saranjil Amarsaikhan <115115572+Saranjil@users.noreply.github.com>
2024-05-26 18:07:44 +0200

    Create User.cs

---
+++ book_project/Models/User.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookReviewPlatform.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+      
+        public string Username { get; set; }
+
+        
+        public string PasswordHash { get; set; }
+
+        public string Token { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<Favourite> Favourites { get; set; }
+    }
+}
