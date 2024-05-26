08fbe5a6bb8e4e0f64c79901782c345b7e94183e
Saranjil Amarsaikhan <115115572+Saranjil@users.noreply.github.com>
2024-05-26 18:10:00 +0200

    Create Review.cs

---
+++ book_project/Models/Review.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookReviewPlatform.Models
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Analysis { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
