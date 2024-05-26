3ff5c4de5492f4db67820c3960aadb46a5e015ae
Saranjil Amarsaikhan <115115572+Saranjil@users.noreply.github.com>
2024-05-26 18:26:48 +0200

    Update User.cs

--- book_project/Models/User.cs
+++ book_project/Models/User.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace BookReviewPlatform.Models
+namespace book_project.Models
 {
     public class User
     {
